62584c58c9cefb8f75b7970896ee71d694fb8505
dmishchenko <d.mishchenko@netbox.ru>
2017-11-24 10:50:09 +0300

    Добавил сопутствующие товары

--- resources/assets/js/apps/shop/goods/components/EntityEdit.vue
+++ resources/assets/js/apps/shop/goods/components/EntityEdit.vue
@@ -183,7 +183,7 @@
                 Vue.Events.emit('Entity:pre_save');
 
                 this.Model.goods_links_data = this.$refs.goods_links[0].getData();
-               
+
                 let newData = {};
                 for (let i in this.Model) {
                     if (typeof this.BaseModel[i] === 'undefined' || this.BaseModel[i] !== this.Model[i]) {
--- resources/assets/js/apps/shop/goods/components/GoodsLinks.vue
+++ resources/assets/js/apps/shop/goods/components/GoodsLinks.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row" v-show="dataLoaded">
-        <div class="links-filter-selector col-md-6">
-            <h1>Укажите подходящие товары:</h1>
+        <div class="links-filter-selector col-md-7">
+            <h5>Укажите фильтр:</h5>
             <filter-selector
                     ref="filter_selector"
                     :entity="entity"
@@ -10,14 +10,14 @@
                     :ls_storage_key="null">
             </filter-selector>
         </div>
-        <div class="col-md-6">
-            <div style="margin-top:30px;" class="row">
+        <div class="col-md-5">
+            <div class="row">
                 <div class="col-md-6">
-                    <div class="label">Максимальное кол-во товаров:</div>
+                    <h5 class="label">Максимальное кол-во товаров:</h5>
                     <input type="number" class="form-control" v-model="Model.limit">
                 </div>
                 <div class="col-md-6">
-                    <div class="label">Сортировка по умолчанию:</div>
+                    <h5 class="label">Сортировка по умолчанию:</h5>
                     <div class="dropdown">
                         <button type="button" class="btn  btn-secondary dropdown-toggle" data-toggle="dropdown">
                             {{ ((Model.sort) ? Sorting[Model.sort] : 'Укажите сортировку') }}
